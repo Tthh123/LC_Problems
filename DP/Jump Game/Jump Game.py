57bbf06ed16bef78ff5a0454833d718f7783a61c
Tthh123 <61496540+Tthh123@users.noreply.github.com>
2023-04-08 02:04:17 +0800

    Backtracking DP

--- DP/Jump Game/Jump Game.py
+++ DP/Jump Game/Jump Game.py
@@ -1,16 +1,22 @@
 def canJump(nums)->bool:
-    if (len(nums) == 1):
-        return True
-    able = False
-    s1 = 0
-    s2 = 0
-    for i in range(len(nums) - 1):
-        if (nums[i] == 0 and s2 <= i):
-            break
-        s1, s2 = s2, max(s2, i + nums[i])
-        if s2 >= (len(nums) - 1):
-            able = True
+    goal=len(nums)-1
+    for i in range(len(nums)-2,-1,-1):
+        if (nums[i]+i)>=goal:
+            goal=i
+    return goal==0
 
-    return able
+print(canJump([1,2]))
 
-print(canJump([2,3,1,1,4]))
+# if (len(nums) == 1):
+#     return True
+# able = False
+# s1 = 0
+# s2 = 0
+# for i in range(len(nums) - 1):
+#     if (nums[i] == 0 and s2 <= i):
+#         break
+#     s1, s2 = s2, max(s2, i + nums[i])
+#     if s2 >= (len(nums) - 1):
+#         able = True
+#
+# return able
